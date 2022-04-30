568b0011eac722cf369bf90037aae883041b63a7
Town One 10 <info@quivergamesvancouver.com>
2022-04-30 10:48:39 -0700

    Added Damage, Armor, AttackDamage, AttackRange, Speed properties

--- StarterPack/Assets/Scripts/Units/Comp_UnitInfo.cs
+++ StarterPack/Assets/Scripts/Units/Comp_UnitInfo.cs
@@ -7,7 +7,10 @@ public class Comp_UnitInfo : MonoBehaviour
     
     [Header("Unit Info")]
     [SerializeField] public float Health = 20.0f;
-
+    [SerializeField] public float Armor = 10.0f;
+    [SerializeField] public float AttackDamage = 5.0f;
+    [SerializeField] public float AttackRange = 1.0f;
+    [SerializeField] public float Speed = 1.0f;
 
 
 }
