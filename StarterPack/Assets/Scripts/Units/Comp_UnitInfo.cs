db5e4bbd5c383aa8d2917af930e458e202fe4597
Town One 10 <info@quivergamesvancouver.com>
2022-04-30 10:38:54 -0700

    Added temp/basic hamster prefab

---
+++ StarterPack/Assets/Scripts/Units/Comp_UnitInfo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comp_UnitInfo : MonoBehaviour
+{
+    
+    [Header("Unit Info")]
+    [SerializeField] public float Health = 20.0f;
+
+
+
+}
