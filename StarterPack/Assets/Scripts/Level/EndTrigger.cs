ec2cf42a5547e47c6954e92be8d111c6040ad434
Tate Smith <tate.smith.4242@gmail.com>
2022-05-01 11:57:39 -0700

    added endtrigger and one level path

--- StarterPack/Assets/Scripts/Level/EndTrigger.cs
+++ StarterPack/Assets/Scripts/Level/EndTrigger.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public bool IsLevelOver { get; private set; }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Train"))
+        {
+            IsLevelOver = true;
+        }
     }
 }
